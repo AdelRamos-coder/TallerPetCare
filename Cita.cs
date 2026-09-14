65aea23f5f605f48f8d01cc04bed403b5ec0664d
adelramos424 <adel.ramos424@pascualbravo.edu.co>
2026-09-14 18:53:42 -0500

    Namespace Arreglado

--- Cita.cs
+++ Cita.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace TallerPetCare
+namespace PetCar
 {
     public enum EstadoCita
     {
--- TipoMedicamento.cs
+++ TipoMedicamento.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace petcar
+namespace PetCar
 {
     public enum  TipoMedicamento
     {
