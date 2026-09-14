d3cc54a403e16bb2bbcfee7494483367584f34ea
wilsoncaraballo520-afk <wilson.caraballo520@pascualbravo.edu.co>
2026-09-14 11:35:39 -0500

    Update Jaula.cs

--- Jaula.cs
+++ Jaula.cs
@@ -1,15 +1,38 @@
 using System;
 
-namespace TallerPetCare
+namespace PetCar
 {
     public class Jaula
     {
-        public string Codigo { get; init; }
-        public TamanoJaula Tamano { get; init; }
-        public decimal TarifaPorDia { get; init; }
-        public bool Ocupada { get; private set; } = false;
-        public string Ocupante { get; private set; } = string.Empty;
-        public int DiasEstancia { get; private set; } = 0;
+        public required string Codigo { get; init; }
+        public TamanoJaula Tamano {  get; init; }
+        public decimal TarifaDia { get; set; }
 
+        public bool Ocupado { get; set; }
+        public string? NombreMascota { get; set; }
+        public int DiasEstancia { get; set; }
+
+        public Jaula(string codigo, TamanoJaula tamano, decimal tarifaDia)
+        {
+            if (string.IsNullOrEmpty(codigo))
+            {
+                throw new ArgumentException("El codigo de la jaula es obligatorio.");
+            }
+
+            if (TarifaDia <= 0)
+            {
+                throw new ArgumentException("La tarifa por dia debe ser mayor a cero.");
+            }
+
+            Codigo = codigo;
+            Tamano = tamano;
+            TarifaDia = tarifaDia;
+
+            Ocupado = false;
+            NombreMascota = null;
+            DiasEstancia = 0;
+        }
+
+      
     }
 }
