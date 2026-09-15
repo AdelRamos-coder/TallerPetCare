2d2238043974fb9d8a327db733b7e9aa2fb8d2a1
wilson22c <caraballosalgadow@gmail.com>
2026-09-15 00:00:02 -0500

    Refactor Medicamento class properties and constructor

--- Medicamento.cs
+++ Medicamento.cs
@@ -1,18 +1,19 @@
+
 using System;
 
-namespace PetCar
+namespace TallerPetcar
 {
     public class Medicamento
     {
         private const int STOCK_MINIMO = 10;
-        private readonly string CodigoInterno;
-        private readonly TipoMedicamento TipoMedicamento;
-        private string NombreComercial;
-        private string Presentacion;
-        private decimal PrecioUnidad;
-        private int StockActual;
-        private DateTime FechaVencimiento;
-        private bool EnNevera;
+        public string CodigoInterno { get; private init; }
+        public TipoMedicamento TipoMedicamento { get; private init; }
+        public string NombreComercial { get; private set; }
+        public string Presentacion { get; private set; }
+        public decimal PrecioUnidad { get; private set; }
+        public int StockActual { get; private set; }
+        public DateTime FechaVencimiento { get; private set; }
+        public bool EnNevera { get; private set; }
 
         public Medicamento(string codigoInterno, TipoMedicamento tipoMedicamento, string nombreComercial, string presentacion, decimal precioUnidad, int stockActual, DateTime fechaVencimiento, bool enNevera)
         {
@@ -20,6 +21,7 @@ namespace PetCar
             {
                 throw new ArgumentException("El codigo del medicamento es obligatorio.");
             }
+
             CodigoInterno = codigoInterno;
             TipoMedicamento = tipoMedicamento;
             NombreComercial = nombreComercial;
@@ -32,7 +34,7 @@ namespace PetCar
 
         public void RecibirUnidades(int cantidad)
         {
-            if (cantidad < 0)
+            if (cantidad <= 0)
             {
                 throw new ArgumentException("La cantidad a recibir no puede ser negativa.");
             }
